a00e8cbf9443df1aa2d16454ca76dd781bc90875
shadowAnonim <shadow.anonim@yandex.ru>
2023-02-21 07:57:56 +0300

    Table

--- app/src/main/java/com/example/greenhousecontrol/MainActivity.java
+++ app/src/main/java/com/example/greenhousecontrol/MainActivity.java
@@ -11,6 +11,10 @@ import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -19,13 +23,17 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private LineChart[] charts = new LineChart[5];
     private Button windowBtn;
-    boolean windowOpen;
+    private TableLayout tl;
+    private ArrayList<String[]> tableData = new ArrayList();
+    private boolean windowOpen;
     private ArrayList<Entry>[] entries = new ArrayList[5];
+    private int[] lineColors = new int[] {Color.BLUE, Color.GREEN, Color.MAGENTA, Color.RED, Color.BLACK};
     private int maxEntries = 15;
     Handler handler;
     SharedPreferences sharedPreferences;
@@ -50,19 +58,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        tl = findViewById(R.id.table);
+        tableData.add(new String[] { "Время", "Номер", "Показания"});
+        
+
+
         windowBtn = findViewById(R.id.windowBtn);
         windowBtn.setOnClickListener(windowBtnClick);
         windowOpen = sharedPreferences.getBoolean("windowOpen", false);
         windowBtn.setText("Форточка " + (windowOpen ? "открыта" : "закрыта"));
         windowBtn.setBackgroundColor(windowOpen ? Color.GREEN : Color.RED);
 
-        charts[0] = findViewById(R.id.chart);
-        charts[1] = findViewById(R.id.chart1);
-        charts[2] = findViewById(R.id.chart2);
-        charts[3] = findViewById(R.id.chart3);
-        charts[4] = findViewById(R.id.chartAvg);
-
-        for (int i = 0; i < 5; i++)
+        charts[0] = findViewById(R.id.chartAvg);
+       for (int i = 0; i < 5; i++)
             entries[i] = new ArrayList<Entry>();
 
         handler = new Handler();
@@ -91,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
                     e.setX(e.getX() - 4);
             for (int i = 0; i < 4; i++) {
                 try {
-                    float temp = API.getTemperature(i + 1);
+                    float temp = API.getTemperature(i + 1);;
                     if (temp == -1)
                     {
                         showError();
@@ -99,8 +107,12 @@ public class MainActivity extends AppCompatActivity {
                     }
                     tempSum += temp;
                     entries[i].add(new Entry(0, temp));
+                    tableData.add(1, new String[] {Calendar.getInstance().getTime().toString(),
+                            String.valueOf(i + 1), String.valueOf(temp)});
                     if (entries[i].size() > maxEntries)
+                    {
                         entries[i].remove(entries[i].get(0));
+                    }
                 }catch (Exception e)
                 {
                     e.printStackTrace();
@@ -117,7 +129,10 @@ public class MainActivity extends AppCompatActivity {
             }
             else windowBtn.setEnabled(true);
             entries[4].add(new Entry(0, avgTemp));
+            if (entries[4].size() > maxEntries)
+                entries[4].remove(entries[4].get(0));
             drawCharts();
+            drawTables();
 
             handler.postDelayed(updateData, 4000);
         }
@@ -126,16 +141,45 @@ public class MainActivity extends AppCompatActivity {
     void drawCharts()
     {
         try {
+            LineData data = new LineData();
             for (int i = 0; i < 5; i++) {
                 LineDataSet dataset = new LineDataSet(entries[i], i == 4 ? "Средняя температура" : "Датчик " + (i + 1));
-                LineData data = new LineData(dataset);
-                charts[i].setData(data);
-                charts[i].invalidate();
+                dataset.setLineWidth(i == 4 ? 3 : 1);
+                dataset.setColor(lineColors[i]);
+                data.addDataSet(dataset);
+                charts[0].invalidate();
             }
+            charts[0].setData(data);
         }
         catch (Exception e)
         {
             e.printStackTrace();
+        }
+
+    }
+
+    void drawTables()
+    {
+        tl.removeAllViews();
+
+        for (String[] line: tableData) {
+            TableRow tr = new TableRow(this);
+            tr.setBackgroundColor(Color.BLACK);
+            tr.setPadding(0, 0, 0, 2); //Border between rows
+
+            TableRow.LayoutParams llp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+            llp.setMargins(0, 0, 2, 0);//2px right-margin
+            for (String s: line) {
+                LinearLayout cell = new LinearLayout(this);
+                cell.setBackgroundColor(Color.WHITE);
+                cell.setLayoutParams(llp);//2px border on the right for the cell
+                TextView tv = new TextView(this);
+                tv.setText(s);
+                tv.setPadding(0, 0, 4, 3);
+                cell.addView(tv);
+                tr.addView(cell);
+            }
+            tl.addView(tr);
         }
 
     }
